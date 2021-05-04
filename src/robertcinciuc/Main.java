48b21f60e685ab347df99d6df3391bcbad950d58
Robert Cinciuc <robertcinciuc@gmail.com>
2021-05-04 19:01:37 +0200

    feat: add Lists

---
+++ src/robertcinciuc/Main.java
@@ -0,0 +1,49 @@
+package robertcinciuc;
+
+import robertcinciuc.structures.LinkedList;
+import robertcinciuc.structures.Node;
+
+public class Main {
+
+    public static void testLinkedList(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.head = new Node(12);
+
+        linkedList.insertAtStart(11);
+        linkedList.insertAtEnd(13);
+        linkedList.insertAtEnd(69);
+        linkedList.insertAtEnd(61);
+
+        linkedList.insertAfterGiven(linkedList.head, 20);
+
+        Node last = linkedList.head;
+        while(last != null){
+            System.out.println(last.data);
+            last = last.next;
+        }
+        System.out.println();
+
+        linkedList.deleteFirstFoundValue(69);
+
+        last = linkedList.head;
+        while(last != null){
+            System.out.println(last.data);
+            last = last.next;
+        }
+        System.out.println();
+
+        linkedList.deleteAtPos(1);
+
+        last = linkedList.head;
+        while(last != null){
+            System.out.println(last.data);
+            last = last.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+//        testLinkedList();
+
+    }
+}

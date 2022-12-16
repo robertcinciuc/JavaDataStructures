58257996683abaf1768b6816651a1d7607fc1f50
Robert Cinciuc <robertcinciuc@gmail.com>
2022-12-16 09:39:30 +0100

    Added almost sorted frame

---
+++ src/robertcinciuc/problems/AlmostSorted.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Result{
+    public static void almostSorted(List<Integer> arr) {
+    }
+
+}
+
+public class AlmostSorted{
+    public static void main(String[] args){
+        
+    }
+}

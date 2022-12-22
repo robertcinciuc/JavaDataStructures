3bca7f2bff107740a29e80cc612809e1f3aea5ac
Robert Cinciuc <robertcinciuc@gmail.com>
2022-12-23 00:20:11 +0100

    Added implem for fraudulent activity (null ptr at big test)

--- src/robertcinciuc/problems/FraudulentActivity.java
+++ src/robertcinciuc/problems/FraudulentActivity.java
@@ -1,37 +1,100 @@
 package robertcinciuc.problems;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class Result3{
+    private static int maxVal = 200;
+
     public static int activityNotifications(List<Integer> expenditure, int d) {
-        Map<Integer, Integer> sumFreq = createFreqMap();
+        Map<Integer, Integer> valFreq = createFreqMap(expenditure, d);
+        int nbNotif = 0;
+        for(int i = d; i < expenditure.size(); ++i){
 
-        for(int i = 0; i < expenditure.size(); ++i){
-            sumFreq.put(expenditure.get(i), sumFreq.get(expenditure.get(i)) + 1);
+//              Iterate over sum freq and send notification here
+            float median;
+            List<Integer> arrForMedian = new ArrayList<>();
+            for(int j = 0; j <= maxVal; ++j){
+                if(valFreq.containsKey(j)){
+                    if(valFreq.get(j) > 0){
+                        int count = valFreq.get(j);
+                        while(count > 0){
+                            arrForMedian.add(j);
+                            count--;
+                        }
+                    }
+                }
+            }
+            median = getMedianExpenditure(arrForMedian);
 
-            if(i >= d){
-                sumFreq.put(expenditure.get(i - d), sumFreq.get(expenditure.get(i - d)) - 1);
+
+            if(expenditure.get(i) >= 2 * median){
+                nbNotif++;
             }
 
-//            Iterate over sum freq and send notification here
+            valFreq.put(expenditure.get(i - d), valFreq.get(expenditure.get(i - d)) - 1);
+
+            if(valFreq.containsKey(expenditure.get(i))){
+                valFreq.put(expenditure.get(i), valFreq.get(expenditure.get(i)) + 1);
+            }else{
+                valFreq.put(expenditure.get(i), 1);
+            }
+
         }
 
-        return 0;
+        return nbNotif;
     }
 
-    private static Map<Integer, Integer> createFreqMap(){
+    private static Map<Integer, Integer> createFreqMap(List<Integer> expenditure, int d){
         Map<Integer, Integer> expenditureFreq = new HashMap<>();
-        for(int i = 0; i <= 200; ++i){
-            expenditureFreq.put(i, 0);
+        for(int i = 0; i < d; ++i){
+            if(expenditureFreq.containsKey(expenditure.get(i))){
+                expenditureFreq.put(expenditure.get(i), expenditureFreq.get(expenditure.get(i) + 1));
+            }else{
+                expenditureFreq.put(expenditure.get(i), 1);
+            }
         }
         return expenditureFreq;
+    }
+
+    private static float getMedianExpenditure(List<Integer> arr) {
+        int size = arr.size();
+        if (size % 2 == 0) {
+            return (arr.get(size / 2) + arr.get((size / 2) - 1)) / 2f;
+        }
+        return arr.get(size / 2);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> expenditure = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int result = Result3.activityNotifications(expenditure, d);
+        System.out.println(result);
+
+        bufferedReader.close();
     }
 }
 
 public class FraudulentActivity {
     public static void main(String[] args){
-        Result3.activityNotifications(null, 0);
+        System.out.println(Result3.activityNotifications(List.of(10, 20, 30, 40, 50), 3));
+        System.out.println(Result3.activityNotifications(List.of(2, 3, 4, 2, 3, 6, 8, 4, 5), 5));
+        System.out.println(Result3.activityNotifications(List.of(2, 3, 4, 2, 3, 6, 8, 4, 5), 4));
+        System.out.println(Result3.activityNotifications(List.of(1, 2, 3, 4, 4), 4));
     }
 }

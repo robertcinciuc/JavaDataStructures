3a893851e727b6beb96d74c202cbfdde6a139607
Robert Cinciuc <robertcinciuc@gmail.com>
2023-01-13 00:37:25 +0100

    Added partial solution (half tests pass) for leetcode coin change

---
+++ src/robertcinciuc/problems/leetcode/CoinChange.java
@@ -0,0 +1,48 @@
+package robertcinciuc.problems.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        Map<Long, Integer> sumMinCoins = new HashMap<>();
+
+        List<Integer> collect = Arrays.stream(coins).boxed().map(e -> e * -1).sorted().collect(Collectors.toList());
+        List<Integer> sortedDescending = collect.stream().map(e -> e * -1).sorted().collect(Collectors.toList());
+        int[] coinsDesc = sortedDescending.stream().mapToInt(e -> e).toArray();
+
+        makeChange(coinsDesc, amount, sumMinCoins, 0, 0);
+
+        return sumMinCoins.getOrDefault((long)amount, -1);
+    }
+
+    public void makeChange(int[] coins, int amount, Map<Long, Integer> sumMinCoins, int nbCoinsSoFar, long sumSoFar){
+        if(sumSoFar <= amount){
+            if(sumMinCoins.containsKey(sumSoFar)) {
+                if(sumMinCoins.get(sumSoFar) > nbCoinsSoFar){
+                    sumMinCoins.put(sumSoFar, nbCoinsSoFar);
+                }else{
+                    return;
+                }
+            }else{
+                sumMinCoins.put(sumSoFar, nbCoinsSoFar);
+            }
+
+            for(int coin : coins){
+                if( sumSoFar + coin <= amount){
+                    makeChange(coins, amount, sumMinCoins, nbCoinsSoFar + 1, sumSoFar + coin);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        CoinChange coinChange = new CoinChange();
+        System.out.println(coinChange.coinChange(new int[]{1,2,5}, 11));
+        System.out.println(coinChange.coinChange(new int[]{1}, 0));
+        System.out.println(coinChange.coinChange(new int[]{1,2147483647}, 2));
+    }
+}

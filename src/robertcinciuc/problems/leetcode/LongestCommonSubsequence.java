e0944ceae3116131e2cf739613529b5c9a948d62
Robert Cinciuc <robertcinciuc@gmail.com>
2023-01-22 13:06:47 +0100

    Added implem for leetcode longest common subsequence by reducing the problem to longest increasing subsequence

--- src/robertcinciuc/problems/leetcode/LongestCommonSubsequence.java
+++ src/robertcinciuc/problems/leetcode/LongestCommonSubsequence.java
@@ -1,8 +1,11 @@
 package robertcinciuc.problems.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LongestCommonSubsequence {
 
-    public int longestCommonSubsequence(String text1, String text2) {
+    public int longestCommonSubsequence0(String text1, String text2) {
         int[] foundPos = new int[text1.length()];
         int[] lens = new int[text1.length()];
 
@@ -14,7 +17,7 @@ public class LongestCommonSubsequence {
             }
         }
 
-            for(int i = 0; i < text1.length(); ++i){
+        for(int i = 0; i < text1.length(); ++i){
             for(int j = 0; j < i; ++j){
 
                 int foundPrevLen = -1;
@@ -58,6 +61,76 @@ public class LongestCommonSubsequence {
         return resp;
     }
 
+    public int longestCommonSubsequence(String text1, String text2){
+
+        int[] twinFromText1 = new int[text2.length()];
+        for(int i = 0; i < text2.length(); ++i){
+            twinFromText1[i] = -1;
+        }
+
+        for(int i = 0; i < text1.length(); ++i){
+            for(int j = 0; j < text2.length(); ++j){
+                if(text1.charAt(i) == text2.charAt(j) && twinFromText1[j] == -1){
+                    twinFromText1[j] = i;
+                    break;
+                }
+            }
+        }
+
+        List<Integer> filteredArrayList = new ArrayList<>();
+        for(int i = 0; i < text2.length(); ++i){
+            if(twinFromText1[i] != -1){
+                filteredArrayList.add(twinFromText1[i]);
+            }
+        }
+
+        int[] ints = filteredArrayList.stream().mapToInt(e -> e).toArray();
+
+        if(ints.length > 0){
+            return lengthOfLIS(ints);
+        }
+
+        return 0;
+    }
+
+    public int lengthOfLIS(int[] nums) {
+
+        int[] subseqSoFar = new int[nums.length];
+        subseqSoFar[0] = 1;
+        for(int i = 1; i < subseqSoFar.length; ++i){
+            subseqSoFar[i] = -1;
+        }
+
+        for(int i = 1; i < nums.length; ++i){
+
+            boolean foundSmaller = false;
+            int maxSubseq = 0;
+            for(int j = 0; j < i; ++j ){
+                if(nums[j] < nums[i]){
+                    foundSmaller = true;
+                    if(subseqSoFar[j] > maxSubseq){
+                        maxSubseq = subseqSoFar[j];
+                    }
+                }
+            }
+
+            if(foundSmaller){
+                subseqSoFar[i] = maxSubseq + 1;
+            }else{
+                subseqSoFar[i] = 1;
+            }
+        }
+
+        int maxSubseq = 0;
+        for(int i = 0; i < subseqSoFar.length; ++i){
+            if(subseqSoFar[i] > maxSubseq){
+                maxSubseq = subseqSoFar[i];
+            }
+        }
+
+        return maxSubseq;
+    }
+
     public static void main(String[] args){
         LongestCommonSubsequence obj = new LongestCommonSubsequence();
         System.out.println(obj.longestCommonSubsequence("abcde", "ace"));
@@ -66,5 +139,6 @@ public class LongestCommonSubsequence {
         System.out.println(obj.longestCommonSubsequence("bsbininm", "jmjkbkjkv"));
         System.out.println(obj.longestCommonSubsequence("oxcpqrsvwf", "shmtulqrypy"));
         System.out.println(obj.longestCommonSubsequence("bl", "yby"));
+        System.out.println(obj.longestCommonSubsequence("abcba", "abcbcba"));
     }
 }

b088c9005190b20a089c4566936b43aa0d3d7df6
Robert Cinciuc <robertcinciuc@gmail.com>
2023-08-21 13:19:06 +0200

    Added final solution for EditDistance - all tests pass (using distance matrix)

--- src/robertcinciuc/problems/leetcode/dp/EditDistance.java
+++ src/robertcinciuc/problems/leetcode/dp/EditDistance.java
@@ -1,66 +1,56 @@
 package robertcinciuc.problems.leetcode.dp;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class EditDistance {
 
     public int minDistance(String word1, String word2) {
 
-        StringBuilder w1 = new StringBuilder(word1);
-        StringBuilder w2 = new StringBuilder(word2);
-        int[] resp = new int[1];
-        resp[0] = Integer.MAX_VALUE;
-        Map<String, Integer> triedCombinations = new HashMap<>();
+        if(word1.isEmpty() && word2.isEmpty()){
+            return 0;
+        }
 
-        recursiveSearch(w1, w2, 0, 0, 0, resp, triedCombinations);
+        int[][] minDist = new int[word1.length() + 1][word2.length() + 1];
+        for(int i = 0; i <= word1.length(); ++i){
+            minDist[i][0] = i;
+        }
 
-        return resp[0];
+        for(int j = 0; j <= word2.length(); ++j){
+            minDist[0][j] = j;
+        }
+
+        if(word1.isEmpty()){
+            return minDist[0][word2.length()];
+        }
+
+        if(word2.isEmpty()){
+            return minDist[word1.length()][0];
+        }
+
+        for(int i = 1; i <= word1.length(); ++i){
+            for(int j = 1; j <= word2.length(); ++j){
+                minDist[i][j] = -1;
+            }
+        }
+
+        recursiveSearch(1, 1,minDist, word1, word2);
+
+        return minDist[word1.length()][word2.length()];
     }
 
-    public void recursiveSearch(StringBuilder w1, StringBuilder w2, int index1, int index2, int lvl, int[] resp, Map<String, Integer> triedCombinations){
-        triedCombinations.put(w2.toString(), lvl);
-
-//        stop condition
-        if(w1.toString().equals(w2.toString()) && lvl < resp[0]){
-            resp[0] = lvl;
-            return;
-        }
-
-        if(index1 < w1.length() && index2 < w2.length()){
-            if(w1.charAt(index1) == w2.charAt(index2)){
-                recursiveSearch(w1, w2, index1 + 1, index2 + 1, lvl, resp, triedCombinations);
+    public void recursiveSearch(int x, int y, int[][] minDist, String w1, String w2){
+        if(x <= w1.length() && y <= w2.length() && minDist[x - 1][y] != -1 && minDist[x][y - 1] != -1 && minDist[x][y] == -1){
+            if(w1.charAt(x - 1) == w2.charAt(y - 1)){
+                minDist[x][y] = minDist[x - 1][y - 1];
+            }else{
+                minDist[x][y] = Math.min(Math.min(minDist[x - 1][y], minDist[x][y - 1]), minDist[x - 1][y - 1]) + 1;
             }
-        }
 
-        if(index2 < w2.length() && index1 < w1.length()){
-            char originalChar = w2.charAt(index2);
-
-            w2.replace(index2, index2 + 1, String.valueOf(w1.charAt(index1)));
-            if(!triedCombinations.containsKey(w2.toString()) || triedCombinations.get(w2.toString()) > lvl){
-                recursiveSearch(w1, w2, index1 + 1, index2 + 1, lvl + 1, resp, triedCombinations);
-            }
-            w2.replace(index2, index2 + 1, String.valueOf(originalChar));
-        }
-
-        if(index1 < w1.length() && index2 <= w2.length()){
-            w2.insert(index2, w1.charAt(index1));
-            if(!triedCombinations.containsKey(w2.toString()) || triedCombinations.get(w2.toString()) > lvl){
-                recursiveSearch(w1, w2, index1 + 1, index2 + 1, lvl + 1, resp, triedCombinations);
-            }
-            w2.delete(index2, index2 + 1);
-        }
-
-        if(index2 < w2.length()){
-            char originalChar = w2.charAt(index2);
-
-            w2.delete(index2, index2 + 1);
-            if(!triedCombinations.containsKey(w2.toString()) || triedCombinations.get(w2.toString()) > lvl){
-                recursiveSearch(w1, w2, index1, index2, lvl + 1, resp, triedCombinations);
-            }
-            w2.insert(index2, originalChar);
+            recursiveSearch(x + 1, y, minDist, w1, w2);
+            recursiveSearch(x, y + 1, minDist, w1, w2);
+            recursiveSearch(x + 1, y + 1, minDist, w1, w2);
         }
     }
+
+
 
     public static void main(String[] args) {
         var v = new EditDistance();
@@ -69,5 +59,6 @@ public class EditDistance {
         System.out.println(v.minDistance("horse", "ros"));
         System.out.println(v.minDistance("intention", "execution"));
         System.out.println(v.minDistance("sea", "eat"));
+        System.out.println(v.minDistance("", ""));
     }
 }

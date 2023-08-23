323826d50d9130b46d0677726b085438d579a0bb
Robert Cinciuc <robertcinciuc@gmail.com>
2023-08-23 12:00:02 +0200

    Fixed solution for Triangle - all tests pass

--- src/robertcinciuc/problems/leetcode/dp/Triangle.java
+++ src/robertcinciuc/problems/leetcode/dp/Triangle.java
@@ -2,57 +2,41 @@ package robertcinciuc.problems.leetcode.dp;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Triangle {
 
     public int minimumTotal(List<List<Integer>> triangle) {
 
-        Map<String, Integer> sums = new HashMap<>();
+        List<List<Integer>> sums = new ArrayList<>();
         for(int i = 0; i < triangle.size(); ++i){
+            sums.add(new ArrayList<>());
             for(int j = 0; j < triangle.get(i).size(); ++j){
-                sums.put(String.valueOf(i) + j, Integer.MAX_VALUE);
+                sums.get(i).add(Integer.MAX_VALUE);
             }
         }
 
         return recursiveSearch(0, 0, triangle, sums);
     }
 
-    public int recursiveSearch(int i, int j, List<List<Integer>> triangle, Map<String, Integer> sums){
-        if(i == triangle.size() || (i < triangle.size() && j == triangle.get(i).size())){
-            return Integer.MAX_VALUE;
+    public int recursiveSearch(int i, int j, List<List<Integer>> triangle, List<List<Integer>> sums){
+        if(i >= triangle.size() ){
+            return 0;
         }
 
-        if(i == triangle.size() - 1 && j < triangle.get(i).size()){
-            sums.put(String.valueOf(i) + j, triangle.get(i).get(j));
-            return triangle.get(i).get(j);
+        if(j >= triangle.get(i).size()){
+            return 0;
         }
 
-        int d = Integer.MAX_VALUE;
-        int r = Integer.MAX_VALUE;
-        if(i + 1 < triangle.size()){
-
-            if(sums.get(String.valueOf(i + 1) + j) == Integer.MAX_VALUE){
-                recursiveSearch(i + 1, j, triangle, sums);
-            }
-            d = sums.get(String.valueOf(i + 1) + j);
-
-            if(j + 1 < triangle.get(i + 1).size()){
-                int tmpJ = j + 1;
-                if(sums.get(String.valueOf(i + 1) + tmpJ) == Integer.MAX_VALUE){
-                    recursiveSearch(i + 1, j + 1, triangle, sums);
-                }
-
-                r = sums.get(String.valueOf(i + 1) + tmpJ);
-            }
+        if(sums.get(i).get(j) != Integer.MAX_VALUE){
+            return sums.get(i).get(j);
         }
 
+        int d = recursiveSearch(i + 1, j, triangle, sums);
+        int r = recursiveSearch(i + 1, j + 1, triangle, sums);
 
         int sum = triangle.get(i).get(j) + Math.min(d, r);
-        sums.put(String.valueOf(i) + j, sum);
-
+        sums.get(i).set(j, sum);
         return sum;
     }
 

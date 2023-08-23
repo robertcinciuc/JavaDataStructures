07f8e0bacb04a1c0f04e2cc35d8af954191afd26
Robert Cinciuc <robertcinciuc@gmail.com>
2023-08-23 11:41:07 +0200

    Added solution for Triangle - 43/44 tests pass - bottom up - wrong answer on 44th

--- src/robertcinciuc/problems/leetcode/dp/Triangle.java
+++ src/robertcinciuc/problems/leetcode/dp/Triangle.java
@@ -1,52 +1,59 @@
 package robertcinciuc.problems.leetcode.dp;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Triangle {
 
     public int minimumTotal(List<List<Integer>> triangle) {
 
         Map<String, Integer> sums = new HashMap<>();
-        sums.put(String.valueOf(0) + 0, triangle.get(0).get(0));
-
-        int[] res = new int[triangle.get(triangle.size() - 1).size()];
-        for(int j = 0; j < triangle.get(triangle.size() - 1).size(); ++j){
-            res[j] = Integer.MAX_VALUE;
-        }
-
-        recursiveSearch(0, 0, sums, triangle, res);
-
-        int mini = Integer.MAX_VALUE;
-        for (int re : res) {
-            if (re < mini) {
-                mini = re;
+        for(int i = 0; i < triangle.size(); ++i){
+            for(int j = 0; j < triangle.get(i).size(); ++j){
+                sums.put(String.valueOf(i) + j, Integer.MAX_VALUE);
             }
         }
 
-        return mini;
+        return recursiveSearch(0, 0, triangle, sums);
     }
 
-    public void recursiveSearch(int i, int j, Map<String, Integer> sums, List<List<Integer>> triangle, int[] res){
-        if(i == triangle.size() - 1){
-            if(res[j] > sums.get(String.valueOf(i) + j)){
-                res[j] = sums.get(String.valueOf(i) + j);
+    public int recursiveSearch(int i, int j, List<List<Integer>> triangle, Map<String, Integer> sums){
+        if(i == triangle.size() || (i < triangle.size() && j == triangle.get(i).size())){
+            return Integer.MAX_VALUE;
+        }
+
+        if(i == triangle.size() - 1 && j < triangle.get(i).size()){
+            sums.put(String.valueOf(i) + j, triangle.get(i).get(j));
+            return triangle.get(i).get(j);
+        }
+
+        int d = Integer.MAX_VALUE;
+        int r = Integer.MAX_VALUE;
+        if(i + 1 < triangle.size()){
+
+            if(sums.get(String.valueOf(i + 1) + j) == Integer.MAX_VALUE){
+                recursiveSearch(i + 1, j, triangle, sums);
+            }
+            d = sums.get(String.valueOf(i + 1) + j);
+
+            if(j + 1 < triangle.get(i + 1).size()){
+                int tmpJ = j + 1;
+                if(sums.get(String.valueOf(i + 1) + tmpJ) == Integer.MAX_VALUE){
+                    recursiveSearch(i + 1, j + 1, triangle, sums);
+                }
+
+                r = sums.get(String.valueOf(i + 1) + tmpJ);
             }
         }
 
-        if(i < triangle.size() && j < triangle.get(i).size()){
-            if(i + 1 < triangle.size() && (!sums.containsKey(String.valueOf(i + 1) + j) ||
-                    sums.get(String.valueOf(i + 1) + j) > sums.get(String.valueOf(i) + j) + triangle.get(i + 1).get(j))){
-                sums.put(String.valueOf(i + 1) + j, sums.get(String.valueOf(i) + j) + triangle.get(i + 1).get(j));
-                recursiveSearch(i + 1, j, sums, triangle, res);
-            }
 
-            int tmpJ = j + 1;
-            if(i + 1 < triangle.size() && j + 1 < triangle.get(i + 1).size() && (!sums.containsKey(String.valueOf(i + 1) + tmpJ) ||
-                    sums.get(String.valueOf(i + 1) + tmpJ) > sums.get(String.valueOf(i) + j) + triangle.get(i + 1).get(tmpJ))){
-                sums.put(String.valueOf(i + 1) + tmpJ, sums.get(String.valueOf(i) + j) + triangle.get(i + 1).get(tmpJ));
-                recursiveSearch(i + 1, tmpJ, sums, triangle, res);
-            }
-        }
+        int sum = triangle.get(i).get(j) + Math.min(d, r);
+        sums.put(String.valueOf(i) + j, sum);
+
+        return sum;
     }
 
     public static void main(String[] args) {
@@ -58,6 +65,11 @@ public class Triangle {
                         Arrays.asList(3, 4),
                         Arrays.asList(6, 5, 7),
                         Arrays.asList(4, 1, 8, 3)
+                ))));
+        System.out.println(v.minimumTotal(new ArrayList<>(
+                Arrays.asList(
+                        Arrays.asList(-1),
+                        Arrays.asList(-2, -3)
                 ))));
     }
 

5d34366b3501bd917b37a1fb629256e41b32cc1b
Robert Cinciuc <robertcinciuc@gmail.com>
2023-08-24 12:46:09 +0200

    Added solution for KthLargestElementInAnArray - all tests pass

--- src/robertcinciuc/problems/leetcode/divideconquer/KthLargestElementInAnArray.java
+++ src/robertcinciuc/problems/leetcode/divideconquer/KthLargestElementInAnArray.java
@@ -1,33 +1,15 @@
 package robertcinciuc.problems.leetcode.divideconquer;
 
+import java.util.Arrays;
+
 public class KthLargestElementInAnArray {
 
     public int findKthLargest(int[] nums, int k) {
 
-        int[] maxi = new int[k];
-        boolean[] used = new boolean[nums.length];
+        Arrays.sort(nums);
 
-        for(int i = 0; i < k; ++i){
-            maxi[i] = Integer.MIN_VALUE;
-            int index = 0;
-            for (int j = 0; j < nums.length; ++j) {
-                if (i > 0) {
-                    if (nums[j] > maxi[i] && nums[j] <= maxi[i - 1] && !used[j]) {
-                        maxi[i] = nums[j];
-                        index = j;
-                    }
-                } else {
-                    if (nums[j] > maxi[i] && !used[j]) {
-                        maxi[i] = nums[j];
-                        index = j;
-                    }
-                }
-            }
 
-            used[index] = true;
-        }
-
-        return maxi[k - 1];
+        return nums[nums.length - k];
     }
 
     public static void main(String[] args) {

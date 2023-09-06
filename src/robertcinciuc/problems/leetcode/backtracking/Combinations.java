d6d7d6b1545871163a0546261f3e43051795f614
Robert Cinciuc <robertcinciuc@gmail.com>
2023-09-06 21:16:02 +0200

    Added solution for Combinations - all tests pass

--- src/robertcinciuc/problems/leetcode/backtracking/Combinations.java
+++ src/robertcinciuc/problems/leetcode/backtracking/Combinations.java
@@ -4,41 +4,28 @@ import java.util.*;
 
 public class Combinations {
     public List<List<Integer>> combine(int n, int k) {
-        Map<Integer, Boolean> left = new HashMap<>();
-        for(int i = 1; i <= n; i++){
-            left.put(i, true);
-        }
-        Set<Set<Integer>> seen = new HashSet<>();
-        Set<Integer> current = new HashSet<>();
-        List<Set<Integer>> resp = new ArrayList<>();
 
-        recursiveSearch(k, left, seen, current, resp);
+        List<Integer> current = new ArrayList<>();
+        List<List<Integer>> resp = new ArrayList<>();
 
-        List<List<Integer>> respFinal = new ArrayList<>();
-        for(Set<Integer> subResp: resp){
-            respFinal.add(new ArrayList<>(subResp));
-        }
+        recursiveSearch(1, n, k, current, resp);
 
-        return respFinal;
+        return resp;
     }
 
-    public void recursiveSearch(int k, Map<Integer, Boolean> left, Set<Set<Integer>> seen, Set<Integer> current, List<Set<Integer>> resp){
-        if(current.size() == k){
-            resp.add(new HashSet<>(current));
+    public void recursiveSearch(int i, int n, int k, List<Integer> current, List<List<Integer>> resp){
+        if(k == 0){
+            resp.add(List.copyOf(current));
             return;
         }
 
-        for(Integer elem : left.keySet()){
-            if(left.get(elem)){
-                current.add(elem);
-                left.put(elem, false);
-                if(!seen.contains(current)){
-                    seen.add(new HashSet<>(current));
-                    recursiveSearch(k, left, seen, current, resp);
-                }
-                current.remove(elem);
-                left.put(elem, true);
-            }
+        if(i > n){
+            return;
         }
+
+        recursiveSearch(i + 1, n, k, current, resp);
+        current.add(i);
+        recursiveSearch(i + 1, n, k - 1, current, resp);
+        current.remove(current.size() - 1);
     }
 }

7051a17a79b40805de8fb71a7ccc247d278bc334
Robert Cinciuc <robertcinciuc@gmail.com>
2023-09-29 21:43:06 +0200

    Added solution for NonDecreasingSubsequences - all tests pass

--- src/robertcinciuc/problems/leetcode/backtracking/NonDecreasingSubsequences.java
+++ src/robertcinciuc/problems/leetcode/backtracking/NonDecreasingSubsequences.java
@@ -8,48 +8,29 @@ public class NonDecreasingSubsequences {
             return List.of();
         }
 
-        Set<Map<Integer, Integer>> seen = new HashSet<>();
-        Map<Integer, Integer> current = new TreeMap<>();
+        Set<List<Integer>> seen = new HashSet<>();
+        List<Integer> current = new ArrayList<>();
         recursiveSearch(seen, current, -101, -1, 0, nums);
 
-        return transformToResult(seen);
+        return new ArrayList<>(seen);
     }
 
-    public void recursiveSearch(Set<Map<Integer, Integer>> seen, Map<Integer, Integer> current, int last, int index, int count, int[] nums){
+    public void recursiveSearch(Set<List<Integer>> seen, List<Integer> current, int last, int index, int count, int[] nums){
         if(seen.contains(current)){
             return;
         }
 
         if(!seen.contains(current) && count > 1){
-            seen.add(new TreeMap<>(current));
+            seen.add(new ArrayList<>(current));
         }
 
         for(int i = index + 1; i < nums.length; ++i){
             if(nums[i] >= last || last == -101){
-                current.merge(nums[i], 1, Integer::sum);
+                current.add(nums[i]);
                 recursiveSearch(seen, current, nums[i], i, count + 1, nums);
-                current.merge(nums[i], 1, (prev, v) -> prev - v);
+                current.remove(current.size() - 1);
             }
         }
 
-    }
-
-    private static List<List<Integer>> transformToResult(Set<Map<Integer, Integer>> seen){
-        List<List<Integer>> res = new ArrayList<>();
-
-        for(Map<Integer, Integer> mapy: seen){
-            List<Integer> arr = new ArrayList<>();
-            for(Map.Entry<Integer, Integer> entry: mapy.entrySet()){
-                int freq = entry.getValue();
-                while(freq > 0){
-                    arr.add(entry.getKey());
-                    freq--;
-                }
-            }
-
-            res.add(arr);
-        }
-
-        return res;
     }
 }

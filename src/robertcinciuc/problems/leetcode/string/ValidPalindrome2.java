0bd4fb6962796d007fad7a552d5fb298dc9168d9
Robert Cinciuc <robertcinciuc@gmail.com>
2025-02-04 23:31:33 +0100

    Added solution for ValidPalindrome2 - all tests pass

--- src/robertcinciuc/problems/leetcode/string/ValidPalindrome2.java
+++ src/robertcinciuc/problems/leetcode/string/ValidPalindrome2.java
@@ -5,24 +5,26 @@ public class ValidPalindrome2 {
     public boolean validPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
-        boolean offsetUsed = false;
         while (left < right){
             if(s.charAt(left) == s.charAt(right) ){
                 left++;
                 right--;
-            }else if(s.charAt(left + 1) == s.charAt(right) && !offsetUsed){
-                left += 2;
-                right--;
-                offsetUsed = true;
-            }else if(s.charAt(left) == s.charAt(right - 1) && !offsetUsed){
-                left++;
-                right -= 2;
-                offsetUsed = true;
             }else{
-                return false;
+                return checkPalindrome(s, left + 1, right) || checkPalindrome(s, left, right - 1);
             }
         }
 
+        return true;
+    }
+
+    private boolean checkPalindrome(String s, int left, int right){
+        while (left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
         return true;
     }
 
